1b7ad3a8cdd3b9b764292ab98343d6059079034c
erarich <ericksantos@mailfence.com>
2023-06-02 15:01:49 -0300

    Add forms in add_player screen

--- lib/Player/add_player.dart
+++ lib/Player/add_player.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
 
-class AddPlayerPage extends StatelessWidget {
-  const AddPlayerPage({super.key});
+class AddPlayerPage extends StatefulWidget {
+  const AddPlayerPage({Key? key}) : super(key: key);
+
+  @override
+  _AddPlayerPageState createState() => _AddPlayerPageState();
+}
+
+class _AddPlayerPageState extends State<AddPlayerPage> {
+  final TextEditingController textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Add player'),
-          centerTitle: true,
-        ),
-      );
+      appBar: AppBar(
+        title: const Text('Add player'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        color: Colors.white,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: textController,
+              maxLines: 1,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Name',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: textController,
+              maxLines: 1,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Age',
+              ),
+            ),
+          ),
+          ElevatedButton(
+            child: const Text('SAVE PLAYER'),
+            onPressed: () {
+              print("Player saved");
+            },
+          )
+        ]),
+      ));
 }
