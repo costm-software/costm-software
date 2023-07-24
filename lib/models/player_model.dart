c506db4b4a607d389f0d752e7e154156125fda4a
SymonRBL <aleffraniery2018@gmail.com>
2023-07-23 22:17:26 -0300

    feat: add attributes to models

--- lib/models/player_model.dart
+++ lib/models/player_model.dart
@@ -1,17 +1,35 @@
+import 'dart:ffi';
+import 'package:uuid/uuid.dart';
+
 import 'package:uuid/uuid.dart';
 
 class Player {
   String? playerId;
   String? name;
   int? age;
+  String? colourHistory;
+  int? colourDifference;
+  double? score;
+  int? pairingNumber;
 
-  Player({String? playerId, this.name, this.age})
+  Player(
+      {String? playerId,
+      this.name,
+      this.age,
+      this.colourHistory,
+      this.colourDifference,
+      this.score,
+      this.pairingNumber})
       : playerId = playerId ?? const Uuid().v4();
 
   Player.fromJson(Map<String, dynamic> json) {
     playerId = json['player_id'];
     name = json['name'];
     age = json['age'];
+    colourHistory = json['colour_history'];
+    colourDifference = json['colour_difference'];
+    score = json['score'];
+    pairingNumber = json['pairing_number'];
   }
 
   Map<String, dynamic> toJson() {
@@ -19,6 +37,10 @@ class Player {
       'player_id': playerId,
       'name': name,
       'age': age,
+      'colour_history': colourHistory,
+      'colour_difference': colourDifference,
+      'score': score,
+      'pairing_number': pairingNumber,
     };
     return data;
   }
